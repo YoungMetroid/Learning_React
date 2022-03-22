9ddda019345eb87d90972ef4f8106e6f10d6af55
YoungMetroid <39865768+YoungMetroid@users.noreply.github.com>
2022-03-22 08:54:37 -0600

    Learning React Chapter 1
    Understanding the react fundamentals

---
+++ 1_jsx/src/index.js
@@ -0,0 +1,11 @@
+// Import the react and ReactDom libraries
+
+import React from  'react';
+import ReactDom from 'react-dom';
+
+// Create a react component
+
+
+// Take the react component and show it on the screen
+
+
