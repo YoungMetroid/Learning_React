f18ab299c937e64161ca381b0e9a64adf4aab996
YoungMetroid <39865768+YoungMetroid@users.noreply.github.com>
2022-04-11 09:14:09 -0500

    Chapter 2

--- 1_jsx/src/index.js
+++ 1_jsx/src/index.js
@@ -1,11 +1,30 @@
 // Import the react and ReactDom libraries
 
-import React from  'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 // Create a react component
 
+function getButtonText()
+{
+  return 'Click on me!'
+}
+
+const App = function () {
+
+  return (
+      <div>
+        <label className ="label" htmlFor = "name">
+            Enter name:
+        </label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor:'blue',color:'white'}}>
+            {getButtonText()}
+        </button>
+      </div>
+  );
+};
 
 // Take the react component and show it on the screen
 
-
+ReactDom.render(<App />, document.querySelector("#root"));
